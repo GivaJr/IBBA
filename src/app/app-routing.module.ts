dbae044e9e2377cade3ec3be0a68a19ea23d08e6
givanildo.junior <givanildo.junior@cxmail.com.br>
2022-12-12 00:54:25 -0300

    removendo import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListPolesComponent } from './core/modules/ibba/list-poles/list-poles.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'ibba/list-poles' , pathMatch: 'full' }
